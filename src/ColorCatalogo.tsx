59a5c61b94dda361d0b75a0d56b050abbba67951
javier <javc2804@gmail.com>
2024-05-13 21:44:14 -0400

    s

--- src/ColorCatalogo.tsx
+++ src/ColorCatalogo.tsx
@@ -1,18 +1,46 @@
-// Define a type for the props
 type ColorCatalogoProps = {
   colorIndex: number;
 };
 
 export const ColorCatalogo: React.FC<ColorCatalogoProps> = ({ colorIndex }) => {
   const colors = [
-    "#FF0000",
-    "#00FF00",
-    "#0000FF",
-    "#FFFF00",
-    "#00FFFF",
-    "#FF00FF",
-  ]; // Colores aleatorios
-  const colorSet = colors.slice(colorIndex, colorIndex + 6);
+    [
+      { code: "#dfe7e9", name: "Blanco" },
+      { code: "#9facb2", name: "Gris" },
+      { code: "#fcc411", name: "Amarillo" },
+      { code: "#3086b5", name: "Azul" },
+      { code: "#c25b52", name: "Rojo" },
+      { code: "#287849", name: "Verde" },
+    ], // Colores para la imagen 1
+    [
+      { code: "#e4e7ec", name: "Blanco" },
+      { code: "#9baab3", name: "Gris" },
+      { code: "#fec513", name: "Amarillo" },
+      { code: "#8c372a", name: "Rojo" },
+    ], // Colores para la imagen 3
+    [
+      { code: "#f3f5f4", name: "Blanco" },
+      { code: "#e7ebea", name: "Blanco Ostra" },
+      { code: "#ddd2be", name: "Marfil" },
+      { code: "#e6d820", name: "Amarillo" },
+      { code: "#ffc835", name: "Amarillo C." },
+      { code: "#44c7db", name: "Azul cielo" },
+      { code: "#15549d", name: "Azul" },
+      { code: "#5ba240", name: "Verde Tilo" },
+      { code: "#04a182", name: "Verde" },
+      { code: "#0d722c", name: "Verde Grama" },
+      { code: "#db7254", name: "Naranja" },
+      { code: "#c21a26", name: "Rojo" },
+      { code: "#48201e", name: "Caoba" },
+      { code: "#79311f", name: "Marron Castana" },
+      { code: "#b6c1c7", name: "Gris Claro" },
+      { code: "#a1b3bd", name: "Gris" },
+      { code: "#a1b3bd", name: "Gris Machete" },
+      { code: "#0d0b0e", name: "Negro" },
+    ], // Colores para la imagen 2
+    // Agrega más arrays de colores si tienes más imágenes
+  ];
+  const colorSet = colors[colorIndex];
   return (
     <div
       style={{
@@ -26,7 +54,7 @@ export const ColorCatalogo: React.FC<ColorCatalogoProps> = ({ colorIndex }) => {
       <div
         style={{
           display: "grid",
-          gridTemplateColumns: "repeat(3, 1fr)",
+          gridTemplateColumns: "repeat(4, 1fr)", // Cambia a 4 columnas
           gap: "2px",
         }}
       >
@@ -34,21 +62,32 @@ export const ColorCatalogo: React.FC<ColorCatalogoProps> = ({ colorIndex }) => {
           <div
             key={index}
             style={{
-              backgroundColor: color,
-              width: "100px",
-              height: "100px",
-              borderRadius: "10px",
-              boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
-              transition: "all 0.3s ease",
-              cursor: "pointer",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
             }}
-            onMouseOver={(e) => {
-              e.currentTarget.style.transform = "scale(1.1)";
-            }}
-            onMouseOut={(e) => {
-              e.currentTarget.style.transform = "scale(1)";
-            }}
-          ></div>
+          >
+            <div
+              style={{
+                backgroundColor: color.code,
+                width: "100px",
+                height: "100px",
+                borderRadius: "18px 0 18px 0", // Curva en las esquinas superior izquierda e inferior derecha
+                boxShadow: "0px 0px 10px rgba(0, 0, 0, 0.1)",
+                transition: "all 0.3s ease",
+                cursor: "pointer",
+                margin: "0 10px 10px 0", // Espacio a la derecha y abajo
+              }}
+              onMouseOver={(e) => {
+                e.currentTarget.style.transform = "scale(1.1)";
+              }}
+              onMouseOut={(e) => {
+                e.currentTarget.style.transform = "scale(1)";
+              }}
+            ></div>
+            <p style={{ color: "white", fontSize: "24px" }}>{color.name}</p>
+          </div>
         ))}
       </div>
     </div>
--- src/Image.tsx
+++ src/Image.tsx
@@ -1,18 +1,18 @@
 import { Link } from "react-router-dom";
 import imagen1 from "./assets/1.png";
 import imagen2 from "./assets/6.png";
-import imagen3 from "./assets/3.png";
+import imagen3 from "./assets/4.png";
 
 export const Image = () => {
   return (
     <div>
-      <Link to="/colors/1">
+      <Link to="/colors/0">
         <img src={imagen1} alt="Imagen 1" />
       </Link>
-      <Link to="/colors/2">
+      <Link to="/colors/1">
         <img src={imagen2} alt="Imagen 2" />
       </Link>
-      <Link to="/colors/3">
+      <Link to="/colors/2">
         <img src={imagen3} alt="Imagen 3" />
       </Link>
     </div>
